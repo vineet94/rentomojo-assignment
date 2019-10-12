89d102f9bcd1e8dd73c66cb76495eaad86f0a499
vineet kumar <vineet_94@outlook.com>
2019-10-12 14:20:06 +0530

    added client code

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.use(passport.session());
 authRoutes(app);
 blogRoutes(app);
 commentRoutes(app);
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
